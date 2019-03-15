7db3171f8b489d8dda250c235e09b86438065b4f
tpettingill <32072040+tpettingill@users.noreply.github.com>
2019-03-14 20:19:28 -0400

    Make request from front-end to receive calc result from API

--- public/script.js
+++ public/script.js
@@ -2,19 +2,23 @@ var numberOneEl = document.getElementById('num1');
 var numberTwoEl = document.getElementById('num2');
 var resultEl = document.getElementById('result');
 
-function calc(operator){    
+var http = new Http();
+
+function calc(operator){
     var num1 = parseInt(numberOneEl.value);
     var num2 = parseInt(numberTwoEl.value);
 
-    if(operator === '+'){
-        resultEl.value=num1+num2;
-    } else if(operator === '-') {
-        resultEl.value=num1-num2;
-    } else if(operator === 'X'){
-        resultEl.value=num1*num2;
-    } else if(operator === '/') {
-        resultEl.value=num1/num2;
-    }
+    http
+        .get('/calculate/' + operator + '/' + num1 + '/' + num2)
+        .then(function(res){
+            var result = JSON.parse(res);
+            console.log(res)
+            console.log(result)
+            resultEl.value = result.result;
+        })
+        .catch(function(err){
+            console.error(err)
+        });
 }
 function reset(){
     numberOneEl.value = '';
