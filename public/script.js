3b7e44615c3df4a9ba2390f0be402f10af7ab916
tpettingill <32072040+tpettingill@users.noreply.github.com>
2019-03-14 20:43:26 -0400

    Add error checking to front-end

--- public/script.js
+++ public/script.js
@@ -5,16 +5,28 @@ var resultEl = document.getElementById('result');
 var http = new Http();
 
 function calc(operator){
-    var num1 = parseInt(numberOneEl.value);
-    var num2 = parseInt(numberTwoEl.value);
+    var num1 = numberOneEl.value;
+    var num2 = numberTwoEl.value;
+    
+    if(num1 == '' || num1 === undefined || num2 == '' || num2 === undefined){
+        return alert('Please enter two integers');
+    }
+    
+    if(!operator || (operator !== 'add' && operator !== 'subtract' && operator !== 'divide' && operator !== 'multiply')){
+        return alert('Invalid operator');
+    }
+    
+    num1 = parseInt(num1) || null;
+    num2 = parseInt(num2) || null;
+    
+    if(num1 === null || num2 === null){
+        return alert('Invalid integers');
+    }
 
     http
         .get('/calculate/' + operator + '/' + num1 + '/' + num2)
         .then(function(res){
-            var result = JSON.parse(res);
-            console.log(res)
-            console.log(result)
-            resultEl.value = result.result;
+            resultEl.value = JSON.parse(res).result;
         })
         .catch(function(err){
             console.error(err)
